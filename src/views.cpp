e085df05284de28e6e3fdd429d6306d0d31b68d8
bhaveshsonalkar <bhaveshsonalkar@gmail.com>
2025-02-14 01:57:00 +0530

    minor changes

--- src/views.cpp
+++ src/views.cpp
@@ -2,15 +2,30 @@
 
 HttpResponse test_get(const HttpRequest &request)
 {
-    HttpResponse response;
-    auto params = request.params;
-    std::string name = params["name"];
+    try
+    {
+        HttpResponse response;
+        auto params = request.params;
+        std::string name = "";
+        if (params.find("name") != params.end())
+        {
+            name = params.at("name");
+        }
 
-    response.status_code = 200;
-    response.status_message = "OK";
-    response.body = "Get request received, name: " + name;
+        response.status_code = 200;
+        response.status_message = "OK";
+        response.body = "Get request received, name: " + name;
 
-    return response;
+        return response;
+    }
+    catch (const std::exception &e)
+    {
+        HttpResponse response;
+        response.status_code = 400;
+        response.status_message = "Bad Request";
+        response.body = "Get request received, but no name was provided";
+        return response;
+    }
 }
 
 HttpResponse test_post(const HttpRequest &request)
@@ -22,7 +37,7 @@ HttpResponse test_post(const HttpRequest &request)
         auto data = request.json_body;
         if (data.find("name") != data.end())
         {
-            name = data["name"];
+            name = data.at("name");
         }
 
         response.status_code = 200;
