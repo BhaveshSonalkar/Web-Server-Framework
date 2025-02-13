256b23d4fd89cc9085fd78d8faca9c234955ddef
bhaveshsonalkar <bhaveshsonalkar@gmail.com>
2025-02-14 02:02:17 +0530

    changed buffer size to 1KB from 1MB

--- include/constants.h
+++ include/constants.h
@@ -2,4 +2,4 @@
 
 #define BACKLOG 3 // specifies the maximum number of pending connections
 
-#define BUFFER_SIZE 1024 // specifies the size of the buffer for reading requests (currently 1 MB)
+#define BUFFER_SIZE 1024 // specifies the size of the buffer for reading requests (currently 1 KB)
